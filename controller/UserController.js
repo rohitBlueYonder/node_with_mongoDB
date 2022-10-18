319fe3ae94596aa389b67139190ab7d14cbaeda8
rohitBlueYonder <rohit.rai@jda.com>
2022-10-18 16:06:27 +0530

    updated controller and added routes

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,21 +1,90 @@
 const { request, response } = require("express")
 const UserModel = require("../models/UserModel")
 
-exports.addUser = (request,response) => {
+exports.addUser = (req,res) => {
     const user = new UserModel({
-        fname: request.body.fname,
-        lname: request.body.lname,
-        email: request.body.email,
-        age: request.body.age
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        age: req.body.age
 
     })
 
     user.
     save().
     then( (data) => {
-        response.send(data)    
+        res.send(data)    
     }).
     catch((error) => {
-        response.send(error)
+        res.send(error)
+    })
+}
+
+
+// getAll
+exports.getUsers = (req,res) => {
+    const users = UserModel.find()
+    users.
+        then( (data) => {
+            res.send(data)
+        }).
+        catch( (error) => {
+            res.send(error)
+        })
+}
+
+// getByName (q1)
+exports.getByName = (req,res) => {
+    const users = UserModel.find({fname:req.params.fname})
+    users.
+        then( (data) => {
+            res.send(data)
+        }).
+        catch( (error) => {
+            res.send(error)
+        })
+}
+
+// show custom fields (q2)
+exports.getNameAndAge = (req,res) => {
+    const users = UserModel.find({},{fname:1, age:1})
+    users.
+        then( (data) => {
+            res.status(200).json({
+                message:"Successfully got name and age",
+                data:data,
+            })
+        }).
+        catch( (error) => {
+            res.send(error)
+        })
+}
+
+// udate Age by Name
+
+exports.updateAgeByname=(req,res)=>{
+    UserModel.findOne({fname:"Rohit"},(error,user)=>{
+        if(error) res.send(error)
+        user.age=req.body.age ? req.body.age: user.age
+        user.save((error)=>{
+            if(error) res.send(error)
+            res.status(200).json({
+                message:"Successfully updated and got the name and age",
+                data:user
+            })
+        })
+    })
+}
+
+
+
+// Increment Age by name
+
+exports.updateAgeByName2 = (req,res) => {
+    UserModel.updateOne({fname:"Kajal"}, { $inc : {age : 25}}, (data) => {
+        res.status(200).json({
+            message : "successfully updated Kajal's Age",
+            data : data
+        })
     })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,11 @@ const controller = require("../controller/UserController")
 
 //API paths
 
-router.post("/add-user", controller.addUser)
+router.post("/addUser", controller.addUser)
+router.get("/getAllUsers", controller.getUsers)
+router.get("/getByName/:fname", controller.getByName)
+router.get("/getByNameAndAge", controller.getNameAndAge)
+router.put("/updateAgeByName", controller.updateAgeByname)
+router.put("/updateIncAgeByName", controller.updateAgeByName2)
 
 module.exports = router
